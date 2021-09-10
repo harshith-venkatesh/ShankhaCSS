0e8dcd8f80fe39984d6a6819f808d2a0860f1fc2
Harshith Venkatesh <44545218+harshith-venkatesh@users.noreply.github.com>
2021-09-10 18:05:06 +0530

    fix: image constants are updated with new links

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,4 +1,4 @@
 export const harshithImage =
-	"https://media-exp1.licdn.com/dms/image/C4D03AQF6ao-JV-_YcQ/profile-displayphoto-shrink_200_200/0/1593626128287?e=1622073600&v=beta&t=SBToFaXo3aRc7Lbca8KNEGz_t9KTqfU5AYG5uk_nRNI";
+	"https://harshith-venkatesh.netlify.app/images/avatar.jpeg";
 export const tanayImage =
-	"https://media-exp1.licdn.com/dms/image/C5103AQGVpPvlt0HvkA/profile-displayphoto-shrink_200_200/0/1562407621248?e=1622073600&v=beta&t=f-a1PcAp4G5M75LkvTD-DO57LpiZcmI5ygParJ3eWA4";
+	"https://yt3.ggpht.com/ytc/AKedOLQnvobEXpzHCxB1g-q2aXbRDzSaznFVHo5OdFML=s900-c-k-c0x00ffffff-no-rj";
